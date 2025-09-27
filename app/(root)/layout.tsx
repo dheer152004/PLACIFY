970d1e85f4e55129b341faf112366e7a925eca78
dheer152004 <dheerajkumar152004@outlook.com>
2025-09-27 11:02:52 +0530

    Study Buddy to Placify

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,8 +7,9 @@ const Layout = ({ children }: { children: ReactNode }) => {
     <div className="root-layout">
       <nav>
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">Placify</h2>
+          {/* <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} /> */}
+          <h2 className="text-primary-100">PLACIFY Interview</h2>
+          
         </Link>
       </nav>
 
